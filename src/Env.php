6ffc9bc4cb9003fa5b7556f0f8e7d10df8f25292
Sergey <pmswga@gmail.com>
2022-11-09 21:33:42 +0300

    Add Env class

---
+++ src/Env.php
@@ -0,0 +1,18 @@
+<?php
+
+	namespace Pmswg\Kenv;
+	
+	class Env
+	{
+		
+		static public function parse_env_string(string $env_string) : array
+		{
+			echo "Parse env string";
+		}
+		
+		static public function parse_env_file(string $filename) : array
+		{
+			echo "Parse env file";
+		}
+		
+	}
