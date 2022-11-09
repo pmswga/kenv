3a104b5a1b4828301bf7491ca1cd10b1f6f5129d
Sergey <pmswga@gmail.com>
2022-11-09 21:50:40 +0300

    Added EnvParseHelper

--- src/Env.php
+++ src/Env.php
@@ -1,5 +1,5 @@
 <?php
-
+	declare(strict_types=1);
 	namespace Pmswg\Kenv;
 	
 	class Env
@@ -7,12 +7,86 @@
 		
 		static public function parse_env_string(string $env_string) : array
 		{
-			echo "Parse env string";
+			$content_lines = explode("\n", $env_string);
+
+			if (empty($content_lines)) {
+				return [];
+			}
+
+			$lines = [];
+
+			foreach ($content_lines as $content_line) {
+				$line = clear_string($content_line);
+
+				if (!empty($line)) {
+					$lines []= $line;
+				}
+			}
+
+			$env = [];
+
+			foreach ($lines as $line) {
+				if (!validate_env_entry($line)) {
+					throw new \RuntimeException('Error env file format');
+				}
+
+				$env_var = explode("=", $line, 2);
+
+				if (count($env_var) !== 2) {
+					throw new \RuntimeException('Error env file format');
+				}
+
+				$env[$env_var[0]] = clear_string($env_var[1]);
+			}
+
+			return $env;
 		}
 		
 		static public function parse_env_file(string $filename) : array
-		{
-			echo "Parse env file";
+		{			
+			if (!is_file($filename) && !file_exists($filename)) {
+				throw new \RuntimeException('File is not exists');
+			}
+
+			$content = file_get_contents($filename);
+
+			if (empty($content)) {
+				return [];
+			}
+
+			$content_lines = explode("\n", $content);
+
+			if (empty($content_lines)) {
+				return [];
+			}
+
+			$lines = [];
+
+			foreach ($content_lines as $content_line) {
+				$line = EnvParseHelper::clear_string($content_line);
+
+				if (!empty($line)) {
+					$lines []= $line;
+				}
+			}
+
+			$env = [];
+
+			foreach ($lines as $line) {
+				if (!EnvParseHelper::validate_env_entry($line)) {
+					throw new \RuntimeException('Error env file format');
+				}
+
+				$env_var = explode("=", $line, 2);
+
+				if (count($env_var) !== 2) {
+					throw new \RuntimeException('Error env file format');
+				}
+
+				$env[$env_var[0]] = EnvParseHelper::clear_string($env_var[1]);
+			}
+
+			return $env;
 		}
 		
 	}
---
+++ src/EnvParseHelper.php
@@ -0,0 +1,23 @@
+<?php
+	declare(strict_types=1);
+	namespace Pmswg\Kenv;
+	
+	class EnvParseHelper
+	{
+		
+		static public function validate_env_entry(string $env_entry) : bool
+		{
+			return preg_match('/(^[A-Z]+[A-Z\d_]*)=(.*\n(?=[A-Z])|.*$)/', $env_entry) > 0;
+		}
+		
+		static public function clear_string(string $str) : string
+		{
+			$pure_string = ltrim($str);
+			$pure_string = trim($pure_string);
+			$pure_string = rtrim($pure_string);
+			$pure_string = str_replace('"', '', $pure_string);
+
+			return $pure_string;
+		}
+		
+	}
